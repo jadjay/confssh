2f07a3fe6e3cd523ce0aba0569c6d77a53b7c553
Jadjay Alolise <jerome.avond@alolise.org>
2014-10-20 00:09:50 +0200

    Ajout de la fonction de lecture du fichier config

--- confssh.rb
+++ confssh.rb
@@ -7,6 +7,8 @@ errors = {
   other: [ 2, "" ],
 }
 file = "../config"
+headers = {}
+sections = {}
 
 commands = {
   create: "",
@@ -30,33 +32,40 @@ def exiting( errors, error, debug )
 end
 
 def create( chaine )
-  print "%s : %s\n" %  ['create',chaine]
+  print "%s : %s\n" %  ['create', chaine]
+end
+
+def read( file, headers = {}, sections = {} )
+  print "%s : %s\n" %  ['read', file]
+  return headers, sections
 end
 
 def list( chaine )
-  print "%s : %s\n" %  ['list',chaine]
+  print "%s : %s\n" %  ['list', chaine]
 end
 
 def notify verb, action
-  print "%s : %s\n" %  ['remove',chaine]
+  print "%s : %s\n" %  ['remove', chaine]
 end
 
 def remove( chaine )
-  print "%s : %s\n" %  ['remove',chaine]
+  print "%s : %s\n" %  ['remove', chaine]
 end
 
 def modify( chaine )
-  print "%s : %s\n" %  ['modify',chaine]
+  print "%s : %s\n" %  ['modify', chaine]
 end
 
 
 if args[0].nil? or not commands.member? args[0].to_s.to_sym 
   usage()
-  exiting( errors, :badarg, debug)
+  exiting errors, :badarg, debug
 end
+
+headers, sections = read file, headers, sections
 
 commande = args[0]
 arguments = args[1..-1].join(' ')
-send(commande,arguments)
+send commande, arguments
 
 # vim: ai ts=2 sts=2 et sw=2 ft=ruby
